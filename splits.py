b7f4970584afdfbd52546778f076d34385229eb7
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-10-27 21:53:53 +0200

    Move batch_iter to splits

--- splits.py
+++ splits.py
@@ -60,3 +60,26 @@ def cross_validation(y, x, k_fold, lambda_, degree, seed):
     rmse_te = np.sqrt(2 * avg_mse_te)
 
     return rmse_tr, rmse_te
+
+def batch_iter(y, tx, batch_size, num_batches=1, seed=0):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    np.random.seed(seed)
+    shuffle_indices = np.random.permutation(np.arange(data_size))
+    shuffled_y = y[shuffle_indices]
+    shuffled_tx = tx[shuffle_indices]
+
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
